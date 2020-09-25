d92df3a9d617e5ddf2e3e438db2184bf5937f4ed
Kiril Gudim <goudimlevkovich@gmail.com>
2020-09-25 22:55:50 +0300

    Updates

---
+++ precache-manifest.10d539ef6b43df3ed536d3cf3fe2a553.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "caf7d30bda429d581c825ad8523013bc",
+    "url": "/KirilG-L/werdevs-page.git/index.html"
+  },
+  {
+    "revision": "e73fa6fea804b20c6450",
+    "url": "/KirilG-L/werdevs-page.git/static/css/2.c6231af7.chunk.css"
+  },
+  {
+    "revision": "a22b23bb693d828130e7",
+    "url": "/KirilG-L/werdevs-page.git/static/css/main.5324f62a.chunk.css"
+  },
+  {
+    "revision": "e73fa6fea804b20c6450",
+    "url": "/KirilG-L/werdevs-page.git/static/js/2.f2d00118.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/KirilG-L/werdevs-page.git/static/js/2.f2d00118.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a22b23bb693d828130e7",
+    "url": "/KirilG-L/werdevs-page.git/static/js/main.caa8f914.chunk.js"
+  },
+  {
+    "revision": "a01bffb93db5689954d2",
+    "url": "/KirilG-L/werdevs-page.git/static/js/runtime-main.aa660a11.js"
+  },
+  {
+    "revision": "37a75b54218416da18cdf1fa0650c569",
+    "url": "/KirilG-L/werdevs-page.git/static/media/Artboard.37a75b54.jpg"
+  },
+  {
+    "revision": "b1a38376a7a592cc41e7b6701aada3d1",
+    "url": "/KirilG-L/werdevs-page.git/static/media/Logo.b1a38376.svg"
+  }
+]);
